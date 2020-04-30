7ca9ac319bf4f973187dd1d425c2dd3e58bdf12d
sasaken <sasakendodt3@gmail.com>
2020-04-30 21:56:27 +0900

    letをvarに変更

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,7 +1,7 @@
 // 読み込みが完了したら
 document.addEventListener('turbolinks:load', function () {
   document.querySelector("#btn").addEventListener("click", function() {
-    let status = document.querySelector(".loader.hidden")
+    var status = document.querySelector(".loader.hidden")
     status.classList.remove("hidden");
   });
 });
